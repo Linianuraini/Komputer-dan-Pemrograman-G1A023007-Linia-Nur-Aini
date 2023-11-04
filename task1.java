fffcd4452923f2e4fe4ae505050574afbd70c944
Linianuraini <147375651+Linianuraini@users.noreply.github.com>
2023-11-04 21:49:25 +0700

    Add files via upload

---
+++ task1.java
@@ -0,0 +1,17 @@
+public class task1 {
+    public static void main(String[] args) {
+        String name = "Linia Nur Aini";
+        String NPM = "G1A023007";
+        System.out.println("Nama : " + name);
+        System.out.println("NPM : " + NPM);
+        System.out.println("=================");
+        for (int i = 0 ; i<=100 ; i++){
+            if (i>= 10 ){
+                System.out.println(name);
+            }
+            else{
+                System.out.println(i);
+            }
+        }
+    }
+}
