9803f99e5de58f8747567edc2bad9fef9585c5b4
Linianuraini <147375651+Linianuraini@users.noreply.github.com>
2023-11-04 21:50:39 +0700

    Add files via upload

---
+++ task4.java
@@ -0,0 +1,10 @@
+public class task4 {
+    public static void main(String[] args){
+        String [] matkul = {
+                "fisika, kalkulus, psm, bahasa inggris, ptik"
+        };
+        for (int i=0 ; i<matkul.length; i++){
+            System.out.println(matkul[i]);
+        }
+    }
+}
